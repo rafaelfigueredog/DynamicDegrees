5262fdfd72e064d335f8a46262e8479eb0ea504c
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2025-04-15 21:44:45 -0300

    feat: added reset button and its already working

--- src/containers/styles.jsx
+++ src/containers/styles.jsx
@@ -1,34 +1,39 @@
-import makeStyles  from '@material-ui/styles/makeStyles';
+import makeStyles from '@material-ui/styles/makeStyles';
 
-export const useStyles = makeStyles((theme) => {
-    return ({
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
 
-        root: {
-            display: 'flex',
-            justifyContent: 'flex-start', 
-            alignItems: 'flex-start', 
-            flexDirection: 'row',   
-            marginLeft: 'auto',
-            marginRight: 'auto' 
-        },
+    resetButtonContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: theme.spacing(2), // Espaço abaixo do botão
+        marginTop: theme.spacing(0), // Espaço acima do botão
+        width: '100%', // Garante que o botão fique centralizado na largura total
+    },
 
-        titleColumn: {
-            width: 140, 
-            height: 30 , 
-            margin: 4, 
-            display: "flex",
-            justifyContent: "center",
-            alignItems: 'center',
-            flexDirection: "column", 
-        }, 
+    titleColumn: {
+        width: 140,
+        height: 30,
+        margin: 4,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
 
-        period: {
-            width: 130, 
-            height: 30, 
-            margin: 4, 
-        }
-    });
-})
+    period: {
+        width: 130,
+        height: 30,
+        margin: 4,
+    },
+}));
 
-
-export default useStyles; 
+export default useStyles;
